9b7a32ab9096c275db520e0741a8e7b918b2f7f4
theBappy <abappy575@gmail.com>
2025-11-02 12:15:20 +0600

    deploying through svellla

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,26 @@
 import express from "express";
 import { ENV } from "./libs/env.js";
+import path from "path";
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "API is up and running" });
 });
+app.get("/books", (req, res) => {
+  res.status(200).json({ msg: "This is the books endpoint" });
+});
 
+//make ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(ENV.PORT, () => {
   console.log(`Server is running on port ${ENV.PORT}`);
